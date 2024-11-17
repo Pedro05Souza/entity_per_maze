8744c21e3d13dddf451a0b8aa3dc59b82fd0b3dc
Pedro <pedroh.fsouzars@gmail.com>
2024-11-17 14:29:58 -0300

    fix: graph edges

--- FileHandler.java
+++ FileHandler.java
@@ -10,14 +10,19 @@ public class FileHandler {
         try {
               List<String> lines = Files.readAllLines(Paths.get(file));
 
-              for(int k = 1; k < lines.size(); k++){
+              for(int i = 0; i < lines.size(); i++){
+                lines.set(i, lines.get(i).replaceAll("\\s", ""));
+            }
+              
+              for(int k = 1; k < 2; k++){
+
                   for(int i = 0; i < lines.get(k).length(); i++){
 
-                    if(lines.get(k).charAt(i) == ' '){
-                        continue;
-                    }
-
                     Node node = new Node(lines.get(k).charAt(i));
+                    
+                    if(Character.isUpperCase(node.getValue())){
+                        node.setCharacter(true);
+                    }
 
                     Character nodeValue = Character.toUpperCase(node.getValue());
 
@@ -25,33 +30,31 @@ public class FileHandler {
 
                     String hexa = Converter.convertHexa(nodeValue);
 
-                    for(int j = 0; j < hexa.length(); j++){
+                    for(int j = 0; j < 4; j++){
 
-                        if(hexa.charAt(j) != '0'){
+                        if(hexa.charAt(j) == '1'){
                             continue;
                         }
                         
-                        if(i == 0){
-                            System.out.println("Adding edge to node up: " + node.getValue());
+                        if(j == 0 && k > 1){
                             Node node2 = new Node(lines.get(k - 1).charAt(i));
                             graph.addEdge(node, node2);
                         } 
-                        else if(i == 1){
-                            System.out.println("Adding edge to node right: " + node.getValue());
+                        else if(j == 1 && i < lines.get(k).length() - 1){
                             Node node2 = new Node(lines.get(k).charAt(i + 1));
                             graph.addEdge(node, node2);
                         } 
-                        else if(i == 2){
-                            System.out.println("Adding edge to node down: " + node.getValue());
+                        else if(j == 2 && k < lines.size() - 1){
                             Node node2 = new Node(lines.get(k + 1).charAt(i));
                             graph.addEdge(node, node2);
                         } 
-                        else {
-                            System.out.println("Adding edge to node in left: " + node.getValue());
+                        else if(j == 3 && i > 0){
                             Node node2 = new Node(lines.get(k).charAt(i - 1));
                             graph.addEdge(node, node2);
                         }
                     }
+
+                    System.out.println(graph.getNeighbors(node));
                       
                   }
               }
