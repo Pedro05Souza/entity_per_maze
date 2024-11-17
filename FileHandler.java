8ccffaf19eaa0675cf31f647745ef37c2a825128
Pedro <pedroh.fsouzars@gmail.com>
2024-11-17 01:02:49 -0300

    refact: graph edging logic

--- FileHandler.java
+++ FileHandler.java
@@ -1,30 +1,65 @@
-import java.io.FileReader;
-import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class FileHandler {
 
-    public static void read(FileReader file) {
-        BufferedReader reader = new BufferedReader(file);
+    public static void read(String file) {
         Graph graph = new Graph();
 
         try {
-            reader.readLine(); // pulando a primeira linha
+              List<String> lines = Files.readAllLines(Paths.get(file));
 
-            for(String line = reader.readLine(); line != null; line = reader.readLine()) {
-                for(String character : line.split("")) {
-                    if(character.equals(" ")) {
+              for(int k = 1; k < lines.size(); k++){
+                  for(int i = 0; i < lines.get(k).length(); i++){
+
+                    if(lines.get(k).charAt(i) == ' '){
                         continue;
                     }
-                    
-                    Node node = new Node(character);
-                    String binaryValue = Converter.convertHexa(character);
-                    System.out.println("Character: " + character + " Binary: " + binaryValue);
-                }
-            }
+
+                    Node node = new Node(lines.get(k).charAt(i));
+
+                    Character nodeValue = Character.toUpperCase(node.getValue());
+
+                    graph.addVertex(node);
+
+                    String hexa = Converter.convertHexa(nodeValue);
+
+                    for(int j = 0; j < hexa.length(); j++){
+
+                        if(hexa.charAt(j) != '0'){
+                            continue;
+                        }
+                        
+                        if(i == 0){
+                            System.out.println("Adding edge to node up: " + node.getValue());
+                            Node node2 = new Node(lines.get(k - 1).charAt(i));
+                            graph.addEdge(node, node2);
+                        } 
+                        else if(i == 1){
+                            System.out.println("Adding edge to node right: " + node.getValue());
+                            Node node2 = new Node(lines.get(k).charAt(i + 1));
+                            graph.addEdge(node, node2);
+                        } 
+                        else if(i == 2){
+                            System.out.println("Adding edge to node down: " + node.getValue());
+                            Node node2 = new Node(lines.get(k + 1).charAt(i));
+                            graph.addEdge(node, node2);
+                        } 
+                        else {
+                            System.out.println("Adding edge to node in left: " + node.getValue());
+                            Node node2 = new Node(lines.get(k).charAt(i - 1));
+                            graph.addEdge(node, node2);
+                        }
+                    }
+                      
+                  }
+              }
         } catch (Exception e) {
-            System.out.println("Error reading file");
+            System.out.println("Error reading file" + e);
         }
-        
     }
-    
+
 }
+    
+
