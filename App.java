8038a7ae27d55a22ef8b22ef575b6b4a7c012641
Pedro <pedroh.fsouzars@gmail.com>
2024-11-17 01:02:18 -0300

    refact: main

--- App.java
+++ App.java
@@ -1,11 +1,8 @@
-import java.io.FileReader;
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        FileReader filePath = new FileReader("CD/caso40_2.txt");
-
-        FileHandler.read(filePath);
+        FileHandler.read("CD/caso40_2.txt");	
     }
     
 }
