11340b98d3df5f1446597a46c4cfb72d05d91483
Pedro <pedroh.fsouzars@gmail.com>
2024-11-17 14:29:47 -0300

    refact: remove extra white spaces

--- App.java
+++ App.java
@@ -3,6 +3,5 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
         FileHandler.read("CD/caso40_2.txt");	
-    }
-    
+    }   
 }
