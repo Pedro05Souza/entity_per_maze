e9d56e2ab5ee79c567912b86b32c764ddc1336eb
Pedro <pedroh.fsouzars@gmail.com>
2024-11-25 16:42:18 -0300

    fix: file path

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        FileHandler fileHandler = new FileHandler("casos-5/casos-5/caso40_5a.txt");
+        FileHandler fileHandler = new FileHandler("CD/caso40_2.txt");
         fileHandler.findLabirinthZones();
     }   
 }
